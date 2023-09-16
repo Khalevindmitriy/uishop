fea4fa99baf0ac2ab3eb5ed6b224d0ba4bee2602
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 12:43:20 -0400

    Update webflowitems.js

--- netlify/functions/webflowitems.js
+++ netlify/functions/webflowitems.js
@@ -8,23 +8,15 @@
 <body>
   <script>
     async function fetchWebflowItems() {
-      const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a015840354'; // Replace with your Webflow API key
-      const collectionId = '633cdfe2191b153dc65c63a9'; // Replace with your Webflow collection ID
-      const url = `https://api.webflow.com/collections/${collectionId}/items`;
-
       try {
-        const response = await fetch(url, {
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-          },
-        });
+        const response = await fetch('/.netlify/functions/webflowitems');
 
         if (!response.ok) {
           throw new Error('Error fetching items from Webflow API');
         }
 
         const data = await response.json();
-        console.log('Items from Webflow:', data.items);
+        console.log('Items from Webflow:', data);
       } catch (error) {
         console.error('Error fetching items:', error);
       }
