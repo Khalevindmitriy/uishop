1980d59bddf05b4fc69baf2ea315482d02069ae4
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 12:30:34 -0400

    Update webflowitems.js

--- netlify/functions/webflowitems.js
+++ netlify/functions/webflowitems.js
@@ -11,28 +11,32 @@
   <button onclick="fetchWebflowItems()">Fetch Webflow Items</button>
 
   <script>
-    async function fetchWebflowItems() {
-      const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a015840354'; // Replace with your Webflow API key
-      const collectionId = '633cdfe2191b153dc65c63a9'; // Replace with your Webflow collection ID
-      const url = `https://api.webflow.com/collections/${collectionId}/items`;
+  async function fetchWebflowItems() {
+    const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a015840354'; // Replace with your Webflow API key
+    const collectionId = '633cdfe2191b153dc65c63a9'; // Replace with your Webflow collection ID
+    const url = `https://api.webflow.com/collections/${collectionId}/items`;
 
-      try {
-        const response = await fetch(url, {
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-          },
-        });
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      });
 
-        if (!response.ok) {
-          throw new Error('Error fetching items from Webflow API');
-        }
-
-        const data = await response.json();
-        console.log('Items from Webflow:', data.items);
-      } catch (error) {
-        console.error('Error fetching items:', error);
+      if (!response.ok) {
+        throw new Error('Error fetching items from Webflow API');
       }
+
+      const data = await response.json();
+      console.log('Items from Webflow:', data.items);
+    } catch (error) {
+      console.error('Error fetching items:', error);
     }
-  </script>
+  }
+
+  // Call the function when needed, for example, when a button is clicked
+  document.getElementById('fetchButton').addEventListener('click', fetchWebflowItems);
+</script>
+
 </body>
 </html>
