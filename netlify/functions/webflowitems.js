b06722e4f3299de2e4cb6bb3dc911470a75eee5f
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 12:40:00 -0400

    Update webflowitems.js

--- netlify/functions/webflowitems.js
+++ netlify/functions/webflowitems.js
@@ -3,14 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Webflow Item Fetcher</title>
+  <title>API Page</title>
 </head>
 <body>
-  <h1>Webflow Item Fetcher</h1>
-
   <script>
     async function fetchWebflowItems() {
-      const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a01584035'; // Replace with your Webflow API key
+      const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a015840354'; // Replace with your Webflow API key
       const collectionId = '633cdfe2191b153dc65c63a9'; // Replace with your Webflow collection ID
       const url = `https://api.webflow.com/collections/${collectionId}/items`;
 
@@ -32,7 +30,7 @@
       }
     }
 
-    // Call the function when the window has finished loading
+    // Call the function when the page loads
     window.addEventListener('load', fetchWebflowItems);
   </script>
 </body>
