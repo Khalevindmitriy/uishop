717a3b47997bc477893c6ac5aa004ead095b9d9d
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 12:59:37 -0400

    Update webflowitems.js

--- netlify/functions/webflowitems.js
+++ netlify/functions/webflowitems.js
@@ -3,27 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Include hello.js -->
+  <script src="https://uishop-test.netlify.app/.netlify/functions/hello"></script>
   <title>API Page</title>
 </head>
 <body>
-  <script>
-    async function fetchWebflowItems() {
-      try {
-        const response = await fetch('/.netlify/functions/webflowitems');
-
-        if (!response.ok) {
-          throw new Error(`Error fetching items from Webflow API: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        console.log('Items from Webflow:', data);
-      } catch (error) {
-        console.error('Error fetching items:', error);
-      }
-    }
-
-    // Call the function when the page loads
-    window.addEventListener('load', fetchWebflowItems);
-  </script>
+  <!-- Your existing content -->
 </body>
 </html>
