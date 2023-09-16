f5817a4bb25eb9e71a765c4e7ebbe5ff9f3f7875
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-15 21:11:40 -0400

    Update hello.js

--- netlify-functions/hello.js
+++ netlify-functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Netlify function!" })
+    body: JSON.stringify({ message: "Hello World" })
   };
 };
