9c7b7a6225867bee5358d5d1b21714dd1dccc009
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 13:09:29 -0400

    Update webflowitems.js

--- netlify/functions/webflowitems.js
+++ netlify/functions/webflowitems.js
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Include hello.js -->
-  <script src="https://uishop-test.netlify.app/.netlify/functions/hello"></script>
-  <title>API Page</title>
-</head>
-<body>
-  <!-- Your existing content -->
-</body>
-</html>
+ <script src="https://uishop-test.netlify.app/.netlify/functions/hello"></script>
