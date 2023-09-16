2eef7b589c32fe1ddb800d1b0bfb5b03e1101b16
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 14:08:15 -0400

    Update webflowitems.js

--- netlify/functions/webflowitems.js
+++ netlify/functions/webflowitems.js
@@ -1,28 +1,24 @@
-exports.handler = async (event, context) => {
-  const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a015840354';
-  const collectionId = '633cdfe2191b153dc65c63a9';
-  const url = `https://api.webflow.com/collections/${collectionId}/items`;
+<script>
+    async function fetchWebflowItems() {
+      const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a015840354'; // Replace with your Webflow API key
+      const collectionId = '633cdfe2191b153dc65c63a9'; // Replace with your Webflow collection ID
+      const url = `https://api.webflow.com/collections/${collectionId}/items`;
 
-  try {
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        });
 
-    if (!response.ok) {
-      throw new Error('Error fetching items from Webflow API');
+        if (!response.ok) {
+          throw new Error('Error fetching items from Webflow API');
+        }
+
+        const data = await response.json();
+        console.log('Items from Webflow:', data.items);
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
     }
-
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ items: data.items }), // Wrap data in an object
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
-};
+  </script>
