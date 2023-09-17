becddd3c9004d76ac84ec13ff24c311e51315296
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 23:02:48 -0400

    Update updateagentemail.js

--- netlify/functions/updateagentemail.js
+++ netlify/functions/updateagentemail.js
@@ -1,23 +1,60 @@
-// functions/updateAgentEmail.js
+function updateAgentEmail() {
+  const newAgentEmail = agentEmailInput.value;
 
-exports.handler = async function (event, context) {
-  try {
-    console.log('Update agent email function called.');
+  // Disable the button during the update process
+  updateButton.disabled = true;
+
+  // Show progress indicator
+  progressIndicator.style.display = 'block';
+
+  // Construct the request body
+  const requestBody = {
+    fields: {
+      'agent-email': newAgentEmail
+    }
+  };
+
+  // Replace 'YOUR_ITEM_ID' with the actual item ID you want to update
+  const itemId = 'YOUR_ITEM_ID';
+
+  fetch(`https://api.webflow.com/collections/YOUR_COLLECTION_ID/items/${itemId}`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': 'Bearer YOUR_API_KEY',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Agent email updated:', newAgentEmail);
     
-    // Parse the data from the request body
-    const data = JSON.parse(event.body);
-    console.log('Received data:', data);
+    // Enable the button after the update is complete
+    updateButton.disabled = false;
 
-    // Simulated success response (replace this with actual Webflow API call)
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Agent email updated successfully' }),
-    };
-  } catch (error) {
+    // Hide progress indicator
+    progressIndicator.style.display = 'none';
+
+    // Show confirmation message
+    confirmationMessage.innerHTML = 'Agent email updated successfully!';
+    confirmationMessage.style.display = 'block';
+  })
+  .catch(error => {
     console.error('Error updating agent email:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error updating agent email' }),
-    };
-  }
-};
+    
+    // Enable the button after the update is complete
+    updateButton.disabled = false;
+
+    // Hide progress indicator
+    progressIndicator.style.display = 'none';
+
+    // Show error message
+    confirmationMessage.innerHTML = 'Error updating agent email. Please try again.';
+    confirmationMessage.style.display = 'block';
+  });
+}
