3360af4e01271eba34a7450dd434887efc4cf2ea
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 23:40:39 -0400

    Update updateagentemail.js

--- netlify/functions/updateagentemail.js
+++ netlify/functions/updateagentemail.js
@@ -2,13 +2,11 @@
 
 exports.handler = async function (event, context) {
   try {
-    // Parse the data from the request body
     const data = JSON.parse(event.body);
     console.log('Received data:', data);
 
     // Replace with your actual Webflow API endpoint for updating an item
     const endpoint = `https://api.webflow.com/collections/633cdfe2191b153dc65c63a9/items/63cec1473c544e2f09c4a930`;
-    console.log('Constructed endpoint:', endpoint);
 
     // Replace with your actual Webflow API key
     const apiKey = 'a6f5957da2cbed217ecb43cf800e9a43ee88d6b53dbb73f5660753a015840354';
@@ -21,7 +19,7 @@ exports.handler = async function (event, context) {
       },
       body: JSON.stringify({
         fields: {
-          'agent-email': data.newAgentEmail // Assuming 'agent-email' is the field to update
+          'agent-email': data.newAgentEmail
         }
       })
     });
