d9d73a13f85f44bfcd8f28f1ee470ccb5e7379c2
Dmitriy Khalevin <khalevin1981@gmail.com>
2023-09-16 23:22:18 -0400

    Update updateagentemail.js

--- netlify/functions/updateagentemail.js
+++ netlify/functions/updateagentemail.js
@@ -9,7 +9,7 @@ exports.handler = async function (event, context) {
     // TODO: Use the Webflow API to update the agent's email with the provided data
 
     // Replace with your actual Webflow API endpoint for updating an item
-    const endpoint = `https://api.webflow.com/collections/633cdfe2191b153dc65c63a9/items/63cec1473c544e2f09c4a930`;
+    const endpoint = `https://api.webflow.com/collections/{633cdfe2191b153dc65c63a9}/items/{63cec1473c544e2f09c4a930}`;
     console.log('Constructed endpoint:', endpoint);
 
     const response = await fetch(endpoint, {
